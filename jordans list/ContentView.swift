de89e7ce192e8fb0da04d3798c1d3145d6c34e5d
Jorge Londoño <jorgemedellin1902@gmail.com>
2025-01-15 21:47:47 -0500

    chore: first commit

---
+++ jordans list/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jordans list
+//
+//  Created by Jorge Londoño on 1/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jordans list/jordans_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  jordans_listApp.swift
+//  jordans list
+//
+//  Created by Jorge Londoño on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct jordans_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
